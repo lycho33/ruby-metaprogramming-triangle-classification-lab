9fa73d64859f4baac77f74c2fb7d12cc43a7c716
lycho33 <cho.lyds@gmail.com>
2021-02-19 10:39:34 -0500

    commit tree ce9dee8c2f1bf4882775ca6db528449de8e06a85

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,32 @@
 class Triangle
-  # write code here
+  attr_reader :a, :b, :c
+
+  def initialize(a, b, c)
+    @a = a
+    @b = b
+    @c = c
+  end
+
+  def triangle_checker
+    valid_triangle = [(a + b > c), (a + c > b), (b + c > a)]
+    [a, b, c].each do |side|
+      valid_triangle << false if side <= 0
+      raise TriangleError if valid_triangle.include?(false)
+    end
+  end
+
+  def kind
+    triangle_checker
+    if a == b && b == c
+      return :equilateral
+    elsif a == b || b == c || a == c
+      return :isosceles
+    else
+      return :scalene
+    end
+  end
+  
+  class TriangleError < StandardError
+  end
+
 end
